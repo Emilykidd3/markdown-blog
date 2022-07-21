c31c951abdbfe972c8653f787683c697056dfc64
emilykidd3 <e.kidd61@yahoo.com>
2022-07-20 17:05:38 -0700

    added bootstrap style link, updated index to have title and button to new page, added an array for articles

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const articles = [{
+        title: 'text article',
+        createdAt: Date.now(),
+        description: 'test description'
+    }]
+    res.render('index', {articles: articles })
 })
 
 app.listen(5000);
