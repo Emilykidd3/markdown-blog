86b35f3a93dec511b006a69b10e6571358357db7
emilykidd3 <e.kidd61@yahoo.com>
2022-07-22 20:05:02 -0700

    added new article page and route

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
         createdAt: new Date(),
         description: 'test description'
     }]
-    res.render('index', {articles: articles })
+    res.render('articles/index', {articles: articles })
 })
 
 app.listen(5000);
