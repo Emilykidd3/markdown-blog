d0ebea6e392dbee8a933b03d86cb20e575fc655a
emilykidd3 <e.kidd61@yahoo.com>
2022-08-02 21:23:05 -0700

    added html to show page for buttons and markdown, removed hard coded articles in server and replaced with models article

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Article = require("./models/article")
 const articleRouter = require('./routes/articles');
 const app = express();
 
@@ -9,12 +10,8 @@ app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'text article',
-        createdAt: new Date(),
-        description: 'test description'
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find()
     res.render('articles/index', { articles: articles })
 })
 
