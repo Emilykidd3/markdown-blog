1f8315fe6c3f51aa6d7233e4d62a73d6bbb2841d
emilykidd3 <e.kidd61@yahoo.com>
2022-08-15 16:25:23 -0700

    updated post route to use save article function

--- routes/articles.js
+++ routes/articles.js
@@ -17,20 +17,10 @@ router.get('/:slug', async (req, res) => {
     res.render('articles/show', { article: article })
 })
 
-router.post("/", async (req, res) => {
-  let article = new Article({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown,
-  });
-  try {
-    article = await article.save();
-    res.redirect(`/articles/${article.slug}`)
-  } catch (e) {
-      console.log(e)
-    res.render('articles/new', { article: article })
-  }
-});
+router.post("/", async (req, res, next) => {
+    req.article = new Article();
+    next();
+}, saveArticleAndRedirect('new'));
 
 router.put('/:id', (req, res) => {
   
@@ -42,7 +32,7 @@ router.delete('/:id', async (req, res) => {
 })
 
 function saveArticleAndRedirect(path) {
-  return(req, res) => {
+  return async (req, res) => {
     let article = req.article 
     article.title= req.body.title
     article.description= req.body.description
